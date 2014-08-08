df1c7c9708e6e20a7d6582fec8b6842b0df57611
Bill Hunt <bill@krues8dr.com>
2014-08-08 16:18:50 -0400

    Renaming Internet Explorer in test suite Merge from https://github.com/Jonahss/annotator/commit/9af0d6fbe295f0310093f316b4156e2522604be8

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,9 +9,9 @@ module.exports = function(grunt) {
         options: {
           //username: 'saucelabs-user-name', // if not provided it'll default to ENV SAUCE_USERNAME (if applicable)
           //key: 'saucelabs-key', // if not provided it'll default to ENV SAUCE_ACCESS_KEY (if applicable)
-          sauceConfig: {
-            'selenium-version': '2.41.0'
-          },
+          // sauceConfig: {
+          //   'selenium-version': '2.41.0'
+          // },
           urls: ['localhost:4000/test/runner.html'],
           build: process.env.CI_BUILD_NUMBER,
           testname: 'AnnotatorJS',
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
           // Windows
 
           // XP
-          // { platform: 'XP',          browserName: 'iehta',        version: '8' }, // Doesn't work, IE8 is broken for Selenium.
+          // { platform: 'XP',          browserName: 'internet explorer', version: '8' },
           // { platform: 'XP',          browserName: 'firefox',      version: '10' },
           // { platform: 'XP',          browserName: 'firefox',      version: '11' },
           // { platform: 'XP',          browserName: 'firefox',      version: '12' },
@@ -53,15 +53,15 @@ module.exports = function(grunt) {
           // { platform: 'XP',          browserName: 'googlechrome', version: '34' },
           // { platform: 'XP',          browserName: 'googlechrome', version: '35' },
           // { platform: 'XP',          browserName: 'googlechrome', version: '36' },
-          // { platform: 'XP',          browserName: 'safari',       version: '3'  },
+          // { platform: 'XP',          browserName: 'safari',       version: '3'  }, // Breaking on SL.
           // { platform: 'XP',          browserName: 'safari',       version: '4'  },
-          // { platform: 'XP',          browserName: 'safari',       version: '5'  },
+          // { platform: 'XP',          browserName: 'safari',       version: '5'  }, // Not available in SL for Selenium.
 
           // Windows 7
-          // { platform: 'Windows 7',   browserName: 'iehta',        version: '8'  }, // Doesn't work, IE8 is broken for Selenium.
-          // { platform: 'Windows 7',   browserName: 'iehta',        version: '9'  },
-          // { platform: 'Windows 7',   browserName: 'iehta',        version: '10' },
-          // { platform: 'Windows 7',   browserName: 'iehta',        version: '11' }, // Not available in SL for Selenium.
+          // { platform: 'Windows 7',   browserName: 'internet explorer', version: '8'  },
+          // { platform: 'Windows 7',   browserName: 'internet explorer', version: '9'  },
+          // { platform: 'Windows 7',   browserName: 'internet explorer', version: '10' },
+          // { platform: 'Windows 7',   browserName: 'internet explorer', version: '11' },
           // { platform: 'Windows 7',   browserName: 'firefox',      version: '10' },
           // { platform: 'Windows 7',   browserName: 'firefox',      version: '11' },
           // { platform: 'Windows 7',   browserName: 'firefox',      version: '12' },
@@ -98,7 +98,7 @@ module.exports = function(grunt) {
           // { platform: 'Windows 7',   browserName: 'safari',       version: '5'  },
 
           // Windows 8
-          // { platform: 'Windows 8',   browserName: 'iehta',        version: '10' }, // Not available in SL for Selenium.
+          // { platform: 'Windows 8',   browserName: 'internet explorer', version: '10' },
           // { platform: 'Windows 8',   browserName: 'firefox',      version: '10' },
           // { platform: 'Windows 8',   browserName: 'firefox',      version: '11' },
           // { platform: 'Windows 8',   browserName: 'firefox',      version: '12' },
@@ -134,7 +134,7 @@ module.exports = function(grunt) {
           // { platform: 'Windows 8',   browserName: 'googlechrome', version: '36' },
 
           // Windows 8.1
-          // { platform: 'Windows 8.1', browserName: 'iehta',        version: '11' }, // Not available in SL for Selenium.
+          // { platform: 'Windows 8.1', browserName: 'internet explorer', version: '11' },
           // { platform: 'Windows 8.1', browserName: 'firefox',      version: '10' },
           // { platform: 'Windows 8.1', browserName: 'firefox',      version: '11' },
           // { platform: 'Windows 8.1', browserName: 'firefox',      version: '12' },
@@ -214,37 +214,37 @@ module.exports = function(grunt) {
           // { platform: 'OS X 10.9',   browserName: 'googlechrome', version: '35' },
           ],
 
-          onTestComplete: function(result, callback) {
-            // Called after a unit test is done, per page, per browser
-            // 'result' param is the object returned by the test framework's reporter
-            // 'callback' is a Node.js style callback function. You must invoke it after you
-            // finish your work.
-            // Pass a non-null value as the callback's first parameter if you want to throw an
-            // exception. If your function is synchronous you can also throw exceptions
-            // directly.
-            // Passing true or false as the callback's second parameter passes or fails the
-            // test. Passing undefined does not alter the test result. Please note that this
-            // only affects the grunt task's result. You have to explicitly update the Sauce
-            // Labs job's status via its REST API, if you want so.
+          // onTestComplete: function(result, callback) {
+          //   // Called after a unit test is done, per page, per browser
+          //   // 'result' param is the object returned by the test framework's reporter
+          //   // 'callback' is a Node.js style callback function. You must invoke it after you
+          //   // finish your work.
+          //   // Pass a non-null value as the callback's first parameter if you want to throw an
+          //   // exception. If your function is synchronous you can also throw exceptions
+          //   // directly.
+          //   // Passing true or false as the callback's second parameter passes or fails the
+          //   // test. Passing undefined does not alter the test result. Please note that this
+          //   // only affects the grunt task's result. You have to explicitly update the Sauce
+          //   // Labs job's status via its REST API, if you want so.
 
-            // The example below negates the result, and also updates the Sauce Labs job's status
-            var user = process.env.SAUCE_USERNAME;
-            var pass = process.env.SAUCE_ACCESS_KEY;
-            console.log(result);
-            request.put({
-                url: ['https://saucelabs.com/rest/v1', user, 'jobs', result.job_id].join('/'),
-                auth: { user: user, pass: pass },
-                json: { passed: result.passed }
-            }, function (error, response, body) {
-              if (error) {
-                callback(error);
-              } else if (response.statusCode !== 200) {
-                callback(new Error('Unexpected response status'));
-              } else {
-                callback(null, !result.passed);
-              }
-            });
-          }
+          //   // The example below negates the result, and also updates the Sauce Labs job's status
+          //   var user = process.env.SAUCE_USERNAME;
+          //   var pass = process.env.SAUCE_ACCESS_KEY;
+          //   console.log(result);
+          //   request.put({
+          //       url: ['https://saucelabs.com/rest/v1', user, 'jobs', result.job_id].join('/'),
+          //       auth: { user: user, pass: pass },
+          //       json: { passed: result.passed }
+          //   }, function (error, response, body) {
+          //     if (error) {
+          //       callback(error);
+          //     } else if (response.statusCode !== 200) {
+          //       callback(new Error('Unexpected response status'));
+          //     } else {
+          //       callback(null, !result.passed);
+          //     }
+          //   });
+          // }
 
         }
       }
